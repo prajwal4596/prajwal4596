bed952da118c3638cb0b132858672e856ee17596
Prajwal Gaikwad <prajwalgaikwad4596@gmail.com>
2026-09-03 22:48:39 +0530

    Add files via upload

---
+++ voting.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int age;
+cout<<"Enter you age:";
+cin>>age;
+if (age>=18) 
+cout<<"Eligible";
+else 
+cout<<"Not Eligible";
+return 0;
+}
+
